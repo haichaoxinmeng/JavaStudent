37f78399b6359cadef848febce0f65f6dbafcfde
chenqiang <1987388779@qq.com>
2020-03-02 16:21:09 +0800

    测试

---
+++ src/main/java/corejava/Example1.java
@@ -0,0 +1,20 @@
+package corejava;
+
+/**
+ * @author chenqiang
+ * @create 2020-03-02 16:13
+ */
+public class Example1 {
+
+    public static void main(String[] args) {
+
+        String str1 = "abc";
+
+        String str2 = "abc";
+
+        System.out.println(str1 == str2);
+        System.out.println("abc"== str1);
+
+
+    }
+}
